b36aa71d0f4c0674e72c6377dad2e79e71ca60f4
kalille Overton <>
2022-04-01 13:06:39 -0400

    commit tree 1c360c2880899a9d2ac74ea9567df99504cbb873

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ApplicationRecord
+
+validates :title, presence: true
+validates :content, length:{minimum: 250}
+validates :summary, length:{maximum: 250}
+validates :category, inclusion:{in: ["Fiction", "Non-Fiction"]}
+validate :click_bait
+
+
+BAIT = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+
+  def click_bait
+    if BAIT.none?{
+        |value| value.match title
+    } 
+    errors.add(:title, "must be clickbait")
+end
+
+  end
 end
